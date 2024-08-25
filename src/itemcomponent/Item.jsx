fe2f8f8613cf3e46c5cbe1e7539caea19e6690ba
Manjeet <jaaniraj283@gmail.com>
2024-08-25 18:07:36 +0530

    rr

--- src/itemcomponent/Item.jsx
+++ src/itemcomponent/Item.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import '../component css/home.css'
+import React, { useState, useEffect } from 'react';
+import '../component css/home.css';
 
 function Item() {
-  return (
-    <>
-     <div className="container">
-    <div className="row page5">
-        <div className="col-lg-3 col-md-4 col-sm-6">
-            <div className="boxespg5"></div>
-            <div className="boxespg5"></div>
-            <div className="boxespg5"></div>
-            <div className="boxespg5"></div>
-            <div className="boxespg5"></div>
-        </div>
-    </div>
-</div>
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    </>
-  )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.escuelajs.co/api/v1/products');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <div className="loader"></div>
+      </div>
+    );
+  }
+  return (
+    <div className="container page5">
+      <div className="row">
+        {data.map(item => (
+          <div className="col-md-3 col-sm-4 col-6 page5bx" key={item.id}>
+            <div className="pg5itmbx">
+              <img className='itemimgpag5' src={item.images[0]} alt={item.title} />
+              <p>{item.title}</p>
+              <h4>{item.price} $</h4>
+              <button>More Details</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Item
+export default Item;
